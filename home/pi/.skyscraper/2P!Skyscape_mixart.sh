ea1e87d8462e4a75045f68c5235ec124da8da311
2play <2playbuilds@gmail.com>
2021-05-09 00:02:13 +0200

    New Skyskraper Setup!

---
+++ home/pi/.skyscraper/2P!Skyscape_mixart.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+clear
+	echo 
+	echo " I will display a list of all systems... "
+	echo " If you can't see full list. Use below keys to scroll or exit list!"
+	echo
+	echo "----------------------------------------------------------------------"
+	echo " <space>		Display next k lines of text [current screen size]"
+	echo " <return>		Display next k lines of text [1]*"
+	echo " d			Scroll k lines [current scroll size, initially 11]*"
+	echo " q			Exit from more"
+	echo "----------------------------------------------------------------------"
+	echo
+	echo ***PLEASE TYPE THE SYSTEM NAME AS SHOWS***
+	echo 
+	echo Example: nes
+	echo NOT Nes or NES etc...
+	echo
+	read -n 1 -s -r -p "Press any key to continue..."
+	cd ~/RetroPie/roms/
+	echo
+	ls -d */ | column | more
+	echo
+	read -p 'So which system would you like to Skyscrape?: ' sname
+	echo
+	if [ -d $sname ]; then 
+	Skyscraper -c configMIX.ini -p $sname -s screenscraper --flags unattend,skipped,videos;
+	Skyscraper -c configMIX.ini -p $sname --flags unattend,skipped,nobrackets,relative,videos;
+	rm -rf ~/.skyscraper/cache/$sname
+	clear
+	echo
+	while true; do
+		echo ""
+		read -p 'Whould you like to Skyscrape another system [y] or [n]? ' yn
+		case $yn in
+		[Yy]*) ~/.skyscraper/2Pskyscape.sh;;
+		[Nn]*) exit;;
+		* ) echo ""; echo "Please answer yes or no.";;
+		esac
+	done
+	cd $HOME
+	clear
+	echo
+	echo "[OK DONE!...]"
+	sleep 1
+	fi
+	
