99963999fea52f912e94a938e15b5a67fa871888
2play <2playbuilds@gmail.com>
2022-03-18 11:28:21 +0100

    Update autostart.sh

--- opt/retropie/configs/all/autostart.sh
+++ opt/retropie/configs/all/autostart.sh
@@ -1,7 +1,12 @@
-#fbset -fb /dev/fb0 -g 1920 1080 1920 1080 16
-HDMI2ON=`tvservice -l |grep "HDMI" |wc -l`
-if [[ $HDMI2ON == "2" ]]; then
-/usr/bin/python /opt/retropie/configs/all/PieMarquee2/PieMarquee2.py > /dev/null 2>&1 &
-fi
-#/home/pi/scripts/themerandom.sh
+REGEX_VIDEO="\.avi\|\.mov\|\.mp4\|\.mkv\|\.3gp\|\.mpg\|\.mp3\|\.wav\|\.m4a\|\.aac\|\.ogg\|\.flac"
+REGEX_AUDIO="\.mp3\|\.wav\|\.m4a\|\.aac\|\.ogg\|\.flac"
+
+local rv="$REGEX_VIDEO" >/dev/null 2>&1 &
+local ra="$REGEX_AUDIO" >/dev/null 2>&1 &
+
+mpv --vo=drm ~/RetroPie/splashscreens/PlayBox\ v2\ Intro.mp4 >/dev/null 2>&1 &
+#mpv ~/RetroPie/splashscreens/PlayBox\ v2\ Intro.mp4 >/dev/null 2>&1 &
+#mpv --vo=drm ~/RetroPie/splashscreens/*"$rv" >/dev/null 2>&1 &
+sleep 1; mpv --no-video --loop --shuffle ~/RetroPie/roms/music/*"$ra" >/dev/null 2>&1 &
+#mpv --vo=drm --no-video https://www.youtube.com/watch?v=djV11Xbc914&list=PLmXxqSJJq-yXrCPGIT2gn8b34JjOrl4Xf
 emulationstation --no-splash #auto
