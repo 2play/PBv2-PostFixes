2ea5824d45785965f8098aeefd03198a08d4ec84
2play <2playbuilds@gmail.com>
2022-03-27 00:33:18 +0100

    Clean State

--- home/pi/RetroPie/roms/pico8/+Start PICO8.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/opt/retropie/emulators/pico8/pico8_dyn -root_path /home/pi/RetroPie/roms/pico8 -splore
--- opt/retropie/configs/all/autostart.sh
+++
@@ -1,7 +0,0 @@
-#fbset -fb /dev/fb0 -g 1920 1080 1920 1080 16
-HDMI2ON=`tvservice -l |grep "HDMI" |wc -l`
-if [[ $HDMI2ON == "2" ]]; then
-/usr/bin/python /opt/retropie/configs/all/PieMarquee2/PieMarquee2.py > /dev/null 2>&1 &
-fi
-#/home/pi/scripts/themerandom.sh
-emulationstation --no-splash #auto
