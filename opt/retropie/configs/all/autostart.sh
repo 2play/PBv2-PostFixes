f4d671e77165258032edf6fcaf75092189833e82
2play <2playbuilds@gmail.com>
2022-03-27 00:16:01 +0100

    Update autostart.sh

--- opt/retropie/configs/all/autostart.sh
+++ opt/retropie/configs/all/autostart.sh
@@ -3,4 +3,5 @@
 #local rv="$REGEX_VIDEO" >/dev/null 2>&1 && local ra="$REGEX_AUDIO" >/dev/null 2>&1
 #mpv --shuffle ~/RetroPie/splashscreens/*"$rv" >/dev/null 2>&1
 mpv ~/RetroPie/splashscreens/PlayBox\ v2\ Tinker\ Intro.mp4 >/dev/null 2>&1; sleep 1; mpv --really-quiet --no-video --shuffle ~/RetroPie/roms/music/*.* >/dev/null 2>&1 &
+#/home/pi/scripts/themerandom.sh
 emulationstation --no-spalsh #auto
