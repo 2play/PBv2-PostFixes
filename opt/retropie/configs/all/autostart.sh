8f8a6ac738d6e1e4fc090af226e63736dae1f0e1
2play <2playbuilds@gmail.com>
2022-04-08 22:59:57 +0200

    Update autostart.sh

--- opt/retropie/configs/all/autostart.sh
+++ opt/retropie/configs/all/autostart.sh
@@ -1,5 +1,5 @@
 rm -f ~/.musicpaused.flag
 /home/pi/.local/bin/esbgm > /dev/null 2>&1 &
-mpv ~/RetroPie/splashscreens/PlayBox\ v2\ Tinker\ Intro.mp4 >/dev/null 2>&1
+mpv ~/RetroPie/splashscreens/PlayBox\ v2\ Intro\ RockPro64.mp4 >/dev/null 2>&1
 #/home/pi/scripts/themerandom.sh
 emulationstation --no-splash #auto
