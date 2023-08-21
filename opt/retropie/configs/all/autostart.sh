295a44f311dd3bd2ff575770969a28d7c1e5c66a
2play <sprafs@gmail.com>
2023-08-21 15:01:17 +0200

    Post Updates Clean #1

--- opt/retropie/configs/all/autostart.sh
+++ opt/retropie/configs/all/autostart.sh
@@ -1,5 +1,6 @@
 rm -f ~/.musicpaused.flag
 /home/pi/.local/bin/esbgm > /dev/null 2>&1 &
-mpv ~/RetroPie/splashscreens/PlayBox\ v2\ Intro\ RockPro64.mp4 >/dev/null 2>&1
+ls -1 ~/RetroPie/splashscreens/* | shuf -n1 | mpv -playlist - >/dev/null 2>&1
+#mpv ~/RetroPie/splashscreens/PlayBox\ v2\ Intro\ RockPro64.mp4 >/dev/null 2>&1
 #/home/pi/scripts/themerandom.sh
 emulationstation --no-splash #auto
