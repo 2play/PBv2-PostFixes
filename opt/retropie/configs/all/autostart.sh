e3fa9b8a2d20fa70686be73852819c6e87146828
2play <2playbuilds@gmail.com>
2024-03-24 13:24:57 +0100

    Update autostart.sh

--- opt/retropie/configs/all/autostart.sh
+++ opt/retropie/configs/all/autostart.sh
@@ -1,6 +1,11 @@
 rm -f ~/.musicpaused.flag
 /home/pi/.local/bin/esbgm > /dev/null 2>&1 &
+#fbset -fb /dev/fb0 -g 1920 1080 1920 1080 16
+HDMI2ON=`tvservice -l |grep "HDMI" |wc -l`
+if [[ $HDMI2ON == "2" ]]; then
+/usr/bin/python /opt/retropie/configs/all/PieMarquee2/PieMarquee2.py > /dev/null 2>&1 &
+fi
 ls -1 ~/RetroPie/splashscreens/* | shuf -n1 | mpv -playlist - >/dev/null 2>&1
-#mpv ~/RetroPie/splashscreens/PlayBox\ v2\ Tinker\ Intro.mp4 >/dev/null 2>&1
+#mpv ~/RetroPie/splashscreens/filename.mp4 >/dev/null 2>&1
 #/home/pi/scripts/themerandom.sh
 emulationstation --no-splash #auto
