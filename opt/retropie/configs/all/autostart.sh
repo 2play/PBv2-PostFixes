7e1b07108b3268535b8919e0e8954d49e7425416
2play <sprafs@gmail.com>
2026-03-25 16:45:37 +0100

    03.2026 Postfixes

--- opt/retropie/configs/all/autostart.sh
+++ opt/retropie/configs/all/autostart.sh
@@ -1,11 +1,11 @@
 rm -f ~/.musicpaused.flag
-esbgm > /dev/null 2>&1 &
 #fbset -fb /dev/fb0 -g 1920 1080 1920 1080 16
-HDMI2ON=`tvservice -l |grep "HDMI" |wc -l`
+HDMI2ON=$(grep -c "^connected$" /sys/class/drm/*HDMI*/status | awk -F: '{sum+=$2} END {print sum}')
 if [[ $HDMI2ON == "2" ]]; then
-/usr/bin/python /opt/retropie/configs/all/PieMarquee2/PieMarquee2.py > /dev/null 2>&1 &
+    /usr/bin/python3 /opt/retropie/configs/all/PieMarquee2/PieMarquee2.py > /dev/null 2>&1 &
 fi
 ls -1 ~/RetroPie/splashscreens/* | shuf -n1 | mpv -playlist - >/dev/null 2>&1
+#find ~/RetroPie/splashscreens/ -type f | shuf -n1 | xargs -d '\n' ffplay -fs -autoexit -loglevel quiet >/dev/null 2>&1
 #mpv ~/RetroPie/splashscreens/filename.mp4 >/dev/null 2>&1
 #/home/pi/scripts/themerandom.sh
 emulationstation --no-splash #auto
