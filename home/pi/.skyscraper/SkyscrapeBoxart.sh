2a9aded08ee168d731099a7d0088313c242d7bdf
2play <sprafs@gmail.com>
2026-06-30 11:09:57 +0200

    update Skyscraper script to avoid ES conflict from TTY

--- home/pi/.skyscraper/SkyscrapeBoxart.sh
+++ home/pi/.skyscraper/SkyscrapeBoxart.sh
@@ -3,27 +3,39 @@
 # Watch a video demonstrating the artwork compositing features here: https://youtu.be/TIDD8EFSz50
 # ReadThis For Extra help on manual scrapes: https://github.com/muldjord/skyscraper/blob/master/docs/ARTWORK.md
 # And here https://github.com/muldjord/skyscraper/tree/master?tab=readme-ov-file
+# 30.06.2026
 
-stop_es() {
+function tostop_es() {
     clear
-	echo "[Stopping EmulationStation...]"
-	sleep 1
-	
-	# Stop ES
-    pkill -f emulationstation
-	
-    while pgrep -f emulationstation >/dev/null; do
-        sleep 2
-    done
-
-    # Extra delay to let esbgm service react
-    sleep 1
+    ensure_lolcat
+    echo "[WARN] Please exit EmulationStation manually before you continue with Skyscraper..." | lolcat
+ 	pausepress
 }
 
-start_es() {
-    echo "[Starting EmulationStation...]"
-    nohup emulationstation --no-splash 2>/dev/null &
-    disown
+function tostart_es() {
+    clear
+    ensure_lolcat
+    echo "[WARN] Please start EmulationStation again or restart your system..." | lolcat
+ 	pausepress
+}
+
+
+function pausepress() {
+    echo
+    read -n 1 -s -r -p "Press any key to continue..."
+    echo
+}
+
+
+function ensure_lolcat() {
+    if ! command -v lolcat >/dev/null 2>&1; then
+        sudo gem install lolcat >/dev/null 2>&1
+    else
+        # Test if lolcat runs without Ruby errors
+        if ! echo "test" | lolcat >/dev/null 2>&1; then
+            sudo gem install lolcat >/dev/null 2>&1
+        fi
+    fi
 }
 
 # --- Detect ROM directory ---
@@ -60,7 +72,7 @@ read -p 'So which system would you like to Skyscrape?: ' sname
 echo
 
 if [ -d "$sname" ]; then
-    stop_es
+    tostop_es
     ~/code/skysource/Skyscraper -a ~/.skyscraper/artwork2P.xml -p "$sname" -s screenscraper --flags unattend,skipped,onlymissing,relative,theinfront,videos
     #~/code/skysource/Skyscraper -a ~/.skyscraper/artwork2P.xml -p "$sname" -s screenscraper -u <USER:PASS> --flags unattend,skipped,onlymissing,relative,theinfront,videos
     #~/code/skysource/Skyscraper -a ~/.skyscraper/artwork2P.xml -p "$sname" --flags unattend,skipped,onlymissing,relative,theinfront,videos
@@ -80,5 +92,5 @@ if [ -d "$sname" ]; then
     clear
     echo -e "[OK \033[32mDONE\033[0m!...]"
     sleep 1
-    start_es
+    tostart_es
 fi
